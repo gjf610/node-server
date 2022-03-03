776590fa4da7277940a63b1a6f6d0bc78d66085f
jianfeng <gjf610@yeah.net>
2022-03-03 15:24:00 +0800

    feat: get path

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import * as http from 'http';
 import * as fs from 'fs';
-import * as p from 'path'
+import * as p from 'path';
+import * as url from 'url';
 import { IncomingMessage, ServerResponse } from 'http';
 
 const server = http.createServer();
 const publicDir = p.resolve(__dirname, 'public')
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-
-  const { method, url, headers } = request
-  switch (url) {
+  const { method, url: path, headers } = request
+  const { pathname, search } = url.parse(path)
+  switch (pathname) {
     case '/index.html':
       response.setHeader('Content-Type', 'text/html;charset=utf-8');
       fs.readFile(p.resolve(publicDir, 'index.html'), (err, data) => {
@@ -30,6 +31,9 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
         response.end(data.toString())
       })
       break;
+    default:
+      response.statusCode = 404;
+      response.end()
   }
 })
 
